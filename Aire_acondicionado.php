28fdff796e4a584d1591e644e9a2557832c1db91
rosa <rmoralesmesen@gmail.com>
2026-06-09 23:21:22 -0600

    leccion 3

--- Aire_acondicionado.php
+++ Aire_acondicionado.php
@@ -1,6 +1,6 @@
 <?php require_once "./vistas/vista_superior.php";?>
 
 
-<h1>Inicio</h1>
+<h1>Aire Acondicionado</h1>
 
 <?php require_once "./vistas/vista_inferior.php";?>
--- vistas/vista_superior.php
+++ vistas/vista_superior.php
@@ -1,1 +1,30 @@
-sup
+ <!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./css/main.css">
+        <title>tercera 1</title>
+    </head>
+ <body>
+      <header>
+        <menu>
+
+        <div class="menu_izquierda">
+            <a href="./index.php">
+                <img src="./img/logo.webp" alt="logo">
+            </a>
+        </div>
+
+        <div class="menu_derecha">
+            <a href="./index.php">Inicio</a>
+            <a href="./Aire_acondicionado.php">Aire Acondicionado</a>
+            <a href="./Refrigeracion.php">Refrigeración</a>
+            <a href="./Mantenimiento.php">Mantenimiento</a>
+        </div>
+        
+        </menu>
+      </header>
+
+    
+

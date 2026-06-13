80bba26f9f46bc1f9bda03b84b1ad39a30d151e8
rosa <rmoralesmesen@gmail.com>
2026-06-13 16:48:26 -0600

    leccion 5

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php require_once "./vistas/vista_superior.php";?>
 
-
-<h1>Inicio</h1>
+<div class="main-accordion-container"></div>
+<div class="card-list"></div>
 
 <?php require_once "./vistas/vista_inferior.php";?>
--- vistas/vista_inferior.php
+++ vistas/vista_inferior.php
@@ -8,5 +8,7 @@
 
 
         <script src="./script/hamburger-icon.js"></script>
+        <script src="./script/cards.js"></script>
+        <script src="./script/accordion.js"></script>
     </body>
 </html>
